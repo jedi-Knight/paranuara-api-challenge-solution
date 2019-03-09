596f3d6870ca37138d6176c686780daa31e9f124
Pratik Gautam <gautamxpratik@gmail.com>
2019-03-09 21:42:11 +1100

    bugfixes on .travis and test_http, and adding a timegap of 5 seconds between api server start and endpoint testing;

--- test.sh
+++ test.sh
@@ -12,4 +12,9 @@ pytest -vv --ignore test_http.py
 #Test API Endpoints
 echo "Now starting server at http://0.0.0.0:8080/  .."
 python server.py 2>&1 &
+
+#Now wait for 5 seconds, giving the server some time to start up before continuing tests..
+sleep 5
 pytest -vv test_http.py
+
+
--- test_http.py
+++ test_http.py
@@ -7,15 +7,18 @@ BASE_URL = 'http://0.0.0.0:8080/'
     ('Non Existant', 200, 'application/json')
 ])
 def test_endpoint_company(company_name, expected_response_code, expected_content_type):
+    request = None
     try:
         request = requests.get(BASE_URL + 'company/' + company_name)
-        gotten_response_code = request.status_code
-        gotten_content_type = request.headers['Content-Type']
-
-        assert gotten_response_code == expected_response_code
-        assert gotten_content_type == expected_content_type
     except:
         raise AssertionError('App server not running!')
+
+    gotten_response_code = request.status_code
+    gotten_content_type = request.headers['Content-Type']
+
+    assert gotten_response_code == expected_response_code
+    assert gotten_content_type == expected_content_type
+    
 
 
 
@@ -24,17 +27,18 @@ def test_endpoint_company(company_name, expected_response_code, expected_content
     ('Moon Herring', 'None Existant', 200, 'application/json')
 ])
 def test_endpoint_two_people(person_one, person_two, expected_response_code, expected_content_type):
+    request = None
     try:
         request = requests.get(BASE_URL + 'user/' + person_one + '/' + person_two)
-
-        gotten_response_code = request.status_code
-        gotten_content_type = request.headers['Content-Type']
-
-        assert gotten_response_code == expected_response_code
-        assert gotten_content_type == expected_content_type
     except:
         raise AssertionError('App server not running!')
 
+    gotten_response_code = request.status_code
+    gotten_content_type = request.headers['Content-Type']
+
+    assert gotten_response_code == expected_response_code
+    assert gotten_content_type == expected_content_type
+    
 
 
 
@@ -43,15 +47,18 @@ def test_endpoint_two_people(person_one, person_two, expected_response_code, exp
     ('Non Existant', 200, 'application/json')
 ])
 def test_endpoint_one_person(person_name, expected_response_code, expected_content_type):
+    request = None
     try:
         request = requests.get(BASE_URL + 'user/' + person_name)
-        gotten_response_code = request.status_code
-        gotten_content_type = request.headers['Content-Type']
-
-        assert gotten_response_code == expected_response_code
-        assert gotten_content_type == expected_content_type
     except:
         raise AssertionError('App server not running!')
+
+    gotten_response_code = request.status_code
+    gotten_content_type = request.headers['Content-Type']
+
+    assert gotten_response_code == expected_response_code
+    assert gotten_content_type == expected_content_type
+    
 
 
 
@@ -61,15 +68,18 @@ def test_endpoint_one_person(person_name, expected_response_code, expected_conte
     ('/Invalid/Invalid', 404, 'text/html; charset=UTF-8')
 ])
 def test_endpoint_invalid(invalid_endpoint, expected_response_code, expected_content_type):
+    request = None
     try:
         request = requests.get(BASE_URL + invalid_endpoint)
-        gotten_response_code = request.status_code
-        gotten_content_type = request.headers['Content-Type']
-
-        assert gotten_response_code == expected_response_code
-        assert gotten_content_type == expected_content_type
     except:
         raise AssertionError('App server not running!')
+
+    gotten_response_code = request.status_code
+    gotten_content_type = request.headers['Content-Type']
+
+    assert gotten_response_code == expected_response_code
+    assert gotten_content_type == expected_content_type
+    
 
 
 
@@ -77,12 +87,15 @@ def test_endpoint_invalid(invalid_endpoint, expected_response_code, expected_con
     ('', 200, 'text/html; charset=UTF-8'),
 ])
 def test_endpoint_index_page(index_page, expected_response_code, expected_content_type):
+    request = None
     try:
         request = requests.get(BASE_URL + index_page)
-        gotten_response_code = request.status_code
-        gotten_content_type = request.headers['Content-Type']
-
-        assert gotten_response_code == expected_response_code
-        assert gotten_content_type == expected_content_type
     except:
         raise AssertionError('App server not running!')
+
+    gotten_response_code = request.status_code
+    gotten_content_type = request.headers['Content-Type']
+
+    assert gotten_response_code == expected_response_code
+    assert gotten_content_type == expected_content_type
+        
